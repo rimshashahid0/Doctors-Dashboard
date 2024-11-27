1a39c19991eac237ac10f61adb795703b95f666b
Rimsha <rimshashahid0786@gmail.com>
2024-11-27 23:44:06 +0500

    Dark switch mode JS is added in script.js

--- js/script.js
+++ js/script.js
@@ -78,3 +78,20 @@ document.addEventListener('click', function (event) {
         dropdownMenu.classList.remove('show');
     }
 });
+
+
+
+// ==============================================================================================================================//
+// =========================================================Dark Switch Mode=====================================================// 
+// ==============================================================================================================================//
+
+const switchMode = document.getElementById('switch-mode');
+
+switchMode.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+})
+
